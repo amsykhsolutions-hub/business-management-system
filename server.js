d9c67e2b7797c91a1e3d86849d28fae471138181
Amsykh <amsykhsolutions@gmail.com>
2026-04-21 13:42:38 +0100

    minimal server test

--- server.js
+++ server.js
@@ -1,44 +1,11 @@
-require("dotenv").config();
-
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-
-const connectDB = require("./config/db");
 
 const app = express();
 
-// middleware
-app.use(cors());
-app.use(express.json());
-
-// connect database
-connectDB();
-
-// routes
-app.use("/api/auth", require("./routes/authRoutes"));
-
-// test route
 app.get("/", (req, res) => {
-  res.status(200).send("API is running...");
+  res.send("API is running...");
 });
 
-// protected route
-const authMiddleware = require("./middleware/authMiddleware");
-
-app.get("/api/protected", authMiddleware, (req, res) => {
-  res.json({
-    message: "Protected route accessed",
-    user: req.user
-  });
-});
-
-// DB log (safe version)
-mongoose.connection.on("connected", () => {
-  console.log("🔥 DB Connected:", mongoose.connection.name);
-});
-
-// PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
