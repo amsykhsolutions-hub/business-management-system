4752a103384727b991af798c2e7b7a30432f0af9
Amsykh <amsykhsolutions@gmail.com>
2026-04-26 22:47:00 +0100

    fix order routes completely

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,15 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const orderController = require("../controllers/orderController");
-const { protect } = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
-// DEBUG (remove later)
-console.log("Controller:", orderController);
+// CREATE ORDER
+router.post("/", authMiddleware, orderController.createOrder);
 
-// Get logged-in user's orders
-router.get("/my-orders", protect, orderController.getMyOrders);
-
-// Create order
-router.post("/", protect, orderController.createOrder);
+// GET MY ORDERS
+router.get("/my-orders", authMiddleware, orderController.getMyOrders);
 
 module.exports = router;
