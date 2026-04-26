16ced72408e5946870e6ab89593c89e2db41bf75
Amsykh <amsykhsolutions@gmail.com>
2026-04-26 17:56:52 +0100

    Fix product routes completely

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,10 +5,19 @@ const productController = require("../controllers/productController");
 const { protect } = require("../middleware/authMiddleware");
 const role = require("../middleware/roleMiddleware");
 
-// CREATE PRODUCT (admin only)
+// CREATE PRODUCT (admin)
 router.post("/", protect, role(["admin"]), productController.createProduct);
 
 // GET ALL PRODUCTS
-router.get("/", productController.getProducts);
+router.get("/", productController.getAllProducts);
+
+// GET SINGLE PRODUCT
+router.get("/:id", productController.getProduct);
+
+// UPDATE PRODUCT
+router.put("/:id", protect, role(["admin"]), productController.updateProduct);
+
+// DELETE PRODUCT
+router.delete("/:id", protect, role(["admin"]), productController.deleteProduct);
 
 module.exports = router;
