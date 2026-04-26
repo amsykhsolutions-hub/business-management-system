93165c9e2033f11dab5b1c224d8ab0d765be4067
Amsykh <amsykhsolutions@gmail.com>
2026-04-26 17:15:37 +0100

    Fix admin auth middleware

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authMiddleware");
 const role = require("../middleware/roleMiddleware");
 
 // ADMIN ONLY TEST
-router.get("/test", auth, role(["admin"]), (req, res) => {
+router.get("/test", protect, role(["admin"]), (req, res) => {
   res.json({
     success: true,
     message: "Admin access granted"
